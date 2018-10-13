80e25d3f993c526e254c4a2094fa48c41bd6a65e
suhassobale <suhassobale@gmail.com>
2018-10-13 21:57:41 +0530

    file uploading in node js

---
+++ file-router.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var storage =   multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log( __dirname);
+      cb(null,  './public/upload');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+var upload = multer({storage:storage}).single('filetitle');
+router.get('/',function(req,res,next){
+    res.render('file-upload',{"form":"file upload form"});
+});
+
+router.post('/', function(req,res,next){
+    upload(req, res, function(err) {
+        if(err) {
+            console.log('Error Occured');
+            return;
+          }
+          res.end(JSON.stringify(req.file));
+    });
+     
+});
+
+module.exports=router;
